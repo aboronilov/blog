d94f271e6f9db90df6c7bcdfb2637c6a62d8871e
unknown <boronilov@gmail.com>
2022-05-02 22:25:35 +0200

    React. Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+import Home from "./pages/home/Home";
 import Topbar from "./topbar/TopBar";
 
 function App() {
   return (
     <div>
-      <Topbar />      
+      <Topbar />
+      <Home />      
     </div>
   );
 }
