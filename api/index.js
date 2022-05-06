6380407a905c215b47897bfb1bc7f52c5041ad18
= <=>
2022-05-06 15:29:36 +0200

    Node models created

--- api/index.js
+++ api/index.js
@@ -4,10 +4,6 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
 dotenv.config();
-// mongoose.
-//    connect(process.env.MONGO_URL).
-//    then(console.log('Mongo DB connected')).
-//    catch(err => console.log(err));
 
 try {
    mongoose.connect(process.env.MONGO_URL)
