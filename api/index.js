6dbd0472e11a39a878d85c0f9d4b755082f63106
= <=>
2022-05-11 17:55:03 +0200

    API. categories router

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
 const postRoute = require('./routes/posts')
+const catRoute = require('./routes/categories')
 
 dotenv.config();
 app.use(express.json())
@@ -19,6 +20,7 @@ try {
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/posts", postRoute)
+app.use("/api/categories", catRoute)
 
 app.listen("5000", () => {
    console.log('Backend is running');
