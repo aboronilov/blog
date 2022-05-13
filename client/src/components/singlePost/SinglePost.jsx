ab05f5789c7f415bd6f997f62ce9a2608e88759b
= <=>
2022-05-13 17:33:15 +0200

    Fetched from API - single post

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,27 +1,39 @@
 import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import './singlepost.css';
 
 const Singlepost = () => {
    const location = useLocation();
-   console.log(location.pathname.split("/")[2])
+   const path = location.pathname.split("/")[2]
+   const [post, setPost] = useState({})
+
+   useEffect(() => {
+      const getPost = async () => {
+         const res = await axios.get("/posts/" + path)
+         setPost(res.data)
+      }
+      getPost()
+   }, [path])
+
    return (
       <div className='singlePost'>
          <div className="singlePostWrapper">
-            <img src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="singlePostImg" />
-            <h1 className="singlePostTitle">Lorem ipsum dolor sit amet.
+            {post.photo &&
+               <img src={post.photo} alt="" className="singlePostImg" />
+            }
+            <h1 className="singlePostTitle">
+               {post.title}
                <div className="singlePostEdit">
                   <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
                   <i className="singlePostIcon fa-regular fa-trash-can"></i>
                </div>
             </h1>
             <div className="singlePostInfo">
-               <span className="singlePostAuthor">Author: <b>Jane</b></span>
-               <span className="singlePostDate">1 hour ago</span>
+               <span className="singlePostAuthor">Author: <b>{post.username}</b></span>
+               <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
             </div>
-            <p className="singlePostDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quibusdam laboriosam voluptatibus obcaecati possimus, inventore expedita debitis! Deserunt voluptatum quidem porro delectus inventore, a illum molestiae, cupiditate ea voluptates nobis.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quibusdam laboriosam voluptatibus obcaecati possimus, inventore expedita debitis! Deserunt voluptatum quidem porro delectus inventore, a illum molestiae, cupiditate ea voluptates nobis.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quibusdam laboriosam voluptatibus obcaecati possimus, inventore expedita debitis! Deserunt voluptatum quidem porro delectus inventore, a illum molestiae, cupiditate ea voluptates nobis.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quibusdam laboriosam voluptatibus obcaecati possimus, inventore expedita debitis! Deserunt voluptatum quidem porro delectus inventore, a illum molestiae, cupiditate ea voluptates nobis.</p>
+            <p className="singlePostDesc">{post.desc}</p>
          </div>
       </div>
    );
