007ebd35f17a9df64e3c961631e155e1d34fa1b8
= <=>
2022-05-16 10:29:14 +0200

    fetching user while registration

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,22 +1,59 @@
 import { Link } from 'react-router-dom';
 import './register.css';
+import { useState } from 'react';
+import axios from 'axios'
 
 const Register = () => {
+   const [username, setUsername] = useState("")
+   const [email, setEmail] = useState("")
+   const [password, setPassword] = useState("")
+   const [error, setError] = useState(false)
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      setError(false);
+      try {
+         const res = await axios.post("/auth/register", {
+            username,
+            email,
+            password,
+         });
+         res.data && window.location.replace("/login");
+      } catch (err) {
+         setError(true);
+      }
+   };
+
+   // onChange - лучше не использовать, лучше useRef
+
    return (
       <div className='register'>
          <span className="registerTitle">Register</span>
-         <form className="registerForm">
+         <form className="registerForm" onSubmit={handleSubmit}>
             <label>Username</label>
-            <input type="text" placeholder='Enter your username...' />
+            <input
+               type="text"
+               placeholder='Enter your username...'
+               onChange={(event) => setUsername(event.target.value)}
+            />
             <label>Email</label>
-            <input type="email" placeholder='Enter your email...' />
+            <input
+               type="email"
+               placeholder='Enter your email...'
+               onChange={(event) => setEmail(event.target.value)}
+            />
             <label>Password</label>
-            <input type="password" placeholder='Enter your password...' />
-            <button className="registerButton">Register</button>
+            <input
+               type="password"
+               placeholder='Enter your password...'
+               onChange={(event) => setPassword(event.target.value)}
+            />
+            <button className="registerButton" type="submit">Register</button>
          </form>
          <button className="registerLoginButton">
             <Link to='/login' className='link'>Login</Link>
          </button>
+         {error && <span style={{color: "red", marginTop: "20px"}}>Registration failure</span>}
       </div>
    );
 }
